7fc18eaef81f52b81dfbdcf992ac3e58e36b5811
ArchitAnant <architanant5@gmail.com>
2025-04-17 12:48:18 +0530

    initial commit

---
+++ flurry-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import HeroSecto form './ui/hero/Hero';
+import HeroSection from './ui/hero/Hero';
+
+function App() {
+  return (
+    <div className="App bg-white">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
